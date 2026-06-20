c985c4142cfab246b25b4c3e2852edc1bbb19578
Heitor65 <multheitor@gmail.com>
2026-06-19 21:18:54 -0300

    add: __init__ com funções feitas até agora

---
+++ src/brasildocs/__init__.py
@@ -0,0 +1,7 @@
+from .cpf import validar_cpf, formatar_cpf, gerar_cpf
+from .cnpj import validar_cnpj, formatar_cnpj, gerar_cnpj
+
+__all__ = [
+    "validar_cpf", "formatar_cpf", "gerar_cpf",
+    "validar_cnpj", "formatar_cnpj", "gerar_cnpj"
+]
